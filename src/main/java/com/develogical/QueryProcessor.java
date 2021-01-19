6c6b2d3490970039a82ba90b2a2e4eac614a065f
Joe Rackham <joerackham@hotmail.co.uk>
2021-01-19 11:45:15 +0000

    Add Stephen King query result

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,10 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("king")) {
+            return "Stephen King (21 September 1947 - Present) is an " +
+                    "American author of horror, supernatural fiction, suspense, crime, science-fiction, and fantasy novels.";
+        }
         return "";
     }
 }
